058d3faf2019e69de7edef1d604b15be798ea1c4
anuragrtc25 <anuragrtc25@gmail.com>
2026-01-07 16:00:36 +0530

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,8 +11,8 @@ import {
   SectorInfo,
 } from '../types';
 
-// const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://impactanalysis.rtcknowledge.com/api';
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://impactanalysis.rtcknowledge.com/api';
+// const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api';
 
 class ApiService {
   private getToken(): string | null {
